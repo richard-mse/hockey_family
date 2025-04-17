9c0515fa22e25728b811343a327b8eb7b732e83c
Alec.Schmidt <alecschmidt2013@gmail.com>
2025-04-17 11:24:04 +0200

    feat: rebasing branch

--- lib/src/ui/pages/match/view/match_view_loaded.dart
+++ lib/src/ui/pages/match/view/match_view_loaded.dart
@@ -7,12 +7,23 @@ class _MatchViewLoaded extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = context.read<MatchBloc>();
     final state = bloc.state as MatchLoadedState;
+<<<<<<< HEAD
     // state.games.forEach((game) => {
     //   print(game.gameId)
     // });
+=======
+>>>>>>> 0931220 (feat: able to show an image)
 
+    var gshc_games = state.games.where((game) => game.homeTeamShortName == "GSHC");
+
+    var example_game = gshc_games.first;
+
+    print(example_game.homeTeamShortName);
+    print(example_game.awayTeamShortName);
+
+    
     return const Center(
-      child: Text('Loaded State'),
+      child: Image(image: AssetImage("logos/GSHC.png")),
     );
   }
 }
