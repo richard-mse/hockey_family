ca777aee852045ff72a0b6e5a6ddbe0f465233ca
richard <richarda.tyarks@hes-so.ch>
2025-05-01 15:40:22 +0200

    fix: overflow calendar fix: Singletap trigger date event

--- lib/src/services/firebase.dart
+++ lib/src/services/firebase.dart
@@ -30,6 +30,7 @@ class FireStoreService {
 
   Future<List<Map<String, dynamic>>> getUsers() async {
     try {
+
       final _db = FirebaseFirestore.instance;
       CollectionReference gamesCollection = _db.collection('Users');
 
