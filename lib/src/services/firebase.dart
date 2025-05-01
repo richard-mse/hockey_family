b0c5ce9c036c22a78f16336d3dce7c575ee5b27f
Alec.Schmidt <alecschmidt2013@gmail.com>
2025-05-01 11:58:42 +0200

    feat: Set reservation

--- lib/src/services/firebase.dart
+++ lib/src/services/firebase.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FireStoreService {
 
@@ -43,5 +44,53 @@ class FireStoreService {
     } catch (e) {
       rethrow; // Propagate error
     }
+  }
+
+  Future<List<dynamic>> getGameReservations(String gameId) async {
+    final _db = FirebaseFirestore.instance;
+
+    DocumentReference game = _db.collection("Games").doc(gameId);
+    DocumentSnapshot snapshot = await game.get();
+    final data = snapshot.data();
+
+    if (data == null) {
+      return [];
+    }
+
+    final reservations = data as Map<String, dynamic>;
+
+    return data["Reservations"];
+  }
+
+  Future<void> setGameReservation(String gameId) async {
+    final prefs = await SharedPreferences.getInstance();
+    final _db = FirebaseFirestore.instance;
+
+    final user = prefs.getString("userName");
+
+    DocumentReference game = _db.collection("Games").doc(gameId);
+
+    try {
+      DocumentSnapshot docSnapshot = await game.get();
+
+      if (docSnapshot.exists) {
+        List<dynamic> currentArray = docSnapshot.get('Reservations') ?? [];
+
+        if (currentArray.length < 4) {
+          currentArray.add(user);
+
+          await game.update({'Reservations': currentArray});
+        } else {
+          throw Exception("Game has already 4 reservations");
+        }
+      } else {
+        await game.set({
+          'Reservations': [user],
+        });
+      }
+    } catch (e) {
+      rethrow;
+    }
+
   }
 }
